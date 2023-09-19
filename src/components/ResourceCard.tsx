143da721baea538b8fedebc8015dc70d2717ce91
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-19 11:02:19 +0100

    Add card header as the page link to the resource page

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -7,9 +7,11 @@ import {
   HStack,
   Heading,
   Text,
+  VStack,
 } from "@chakra-ui/react";
 import moment from "moment";
 import { Resource } from "../core/requests/resources";
+import PageLink from "./PageLink";
 import ResourceTagList from "./ResourceTagList";
 
 interface ResourceCardProps {
@@ -17,21 +19,14 @@ interface ResourceCardProps {
 }
 
 function ResourceCard({ resource }: ResourceCardProps) {
-  const handleClick = () => {
-    alert("Redirect to single view not implemented");
-  };
-
   return (
-    <Card
-      minH="20em"
-      as="button"
-      onClick={handleClick}
-      align="center"
-      variant="filled"
-    >
-      <CardHeader>
-        <Heading size="md">{resource.title}</Heading>
-      </CardHeader>
+    <Card minH="20em" align="center" variant="filled">
+      <PageLink page={`/resource/${resource.id}`}>
+        <CardHeader as={VStack} bg="cyan.800" borderTopRadius="6px">
+          <Heading size="md">{resource.title}</Heading>
+        </CardHeader>
+      </PageLink>
+      <Divider />
       <CardBody>
         <Text>{resource.description}</Text>
       </CardBody>
