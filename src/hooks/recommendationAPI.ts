e513e2fc175bbfaddea57e58a7253f33346140f5
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-19 14:41:24 +0100

    Set stale time to one hour instead of Inifinity

--- src/hooks/recommendationAPI.ts
+++ src/hooks/recommendationAPI.ts
@@ -1,11 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getRecommendationOpts } from "../core/requests/resources";
 
+const ONE_HOUR = 3600000;
+
 export const useRecommendationOpts = () => {
   const query = useQuery({
     queryKey: ["recommendationOpts"],
     queryFn: getRecommendationOpts,
-    staleTime: Infinity,
+    staleTime: ONE_HOUR,
   });
 
   return { ...query };
