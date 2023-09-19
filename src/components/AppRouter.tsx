31befa71ffdaa083353f1cfef1bb44316ecbfc90
Carlton Nunes Desouza <carltonlnd@hotmail.com>
2023-09-19 11:01:51 +0100

    Add route for resource page

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,10 +1,14 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "../pages/Home";
+import ResourcePage from "../pages/ResourcePage";
 
 function AppRouter() {
   return (
     <Router>
       <Switch>
+        <Route path="/resource/:id">
+          <ResourcePage />
+        </Route>
         <Route path="/">
           <Home />
         </Route>
